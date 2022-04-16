47d0c762f8d4f607959145400bbfdfa0fc29f0eb
wolfy01 <wolfy3543@gmail.com>
2022-04-16 04:10:53 +0000

    initial commit and updates

---
+++ index.js
@@ -0,0 +1,372 @@
+import WebSocket from "ws";
+import { EventEmitter } from "events";
+
+class Client extends EventEmitter {
+    constructor(uri, token) {
+        super();
+
+        this.uri = uri || "wss://mppclone.com:8443/";
+        this.token = token;
+
+        this.ws = null;
+        this.user = null;
+        this.channel = null;
+        this.pingInterval = null;
+        this.participantId = null;
+        this.connectionTime = null;
+        this.desiredChannelId = null;
+        this.noteFlushInterval = null;
+        this.desiredChannelSettings = null;
+
+        this['🐈'] = 0;
+        this.noteBufferTime = 0;
+        this.serverTimeOffset = 0;
+        this.connectionAttempts = 0;
+
+        this.ppl = {};
+        this.permissions = {};
+
+        this.canConnect = false;
+
+        this.noteBuffer = [];
+
+        this.bindEventListeners();
+        this.emit("status", "(Offline mode)");
+
+        this.offlineSettings = {
+            channel: {
+                color: "#ecfaed"
+            },
+            participant: {
+                _id: "",
+                name: "",
+                color: "#777"
+            }
+        };
+    }
+
+    isConnected() {
+        return this.ws && this.ws.readyState === WebSocket.OPEN;
+    };
+
+    isConnecting() {
+        return this.ws && this.ws.readyState === WebSocket.CONNECTING;
+    };
+
+    start() {
+        this.canConnect = true;
+        if (!this.connectionTime) {
+            this.connect();
+        }
+    };
+
+    stop() {
+        this.canConnect = false;
+        this.ws.close();
+    };
+
+    connect() {
+        if (!this.canConnect || this.isConnected() || this.isConnecting()) return;
+        const self = this;
+
+        this.emit("status", "Connecting...");
+
+        this.ws = new WebSocket(this.uri, {
+            origin: "https://www.multiplayerpiano.com"
+        });
+
+        this.ws.addEventListener("close", function (evt) {
+            self.user = null;
+            self.channel = null;
+            self.participantId = null;
+
+            self.setParticipants([]);
+            clearInterval(self.pingInterval);
+            clearInterval(self.noteFlushInterval);
+
+            self.emit("disconnect", evt);
+            self.emit("status", "Offline mode");
+
+            // reconnect!
+            if (self.connectionTime) {
+                self.connectionTime = null;
+                self.connectionAttempts = 0;
+            } else {
+                ++self.connectionAttempts;
+            }
+
+            const ms_lut = [50, 2500, 10000];
+            let idx = self.connectionAttempts;
+
+            if (idx >= ms_lut.length) idx = ms_lut.length - 1;
+            const ms = ms_lut[idx];
+
+            setTimeout(self.connect.bind(self), ms);
+        });
+        this.ws.addEventListener("error", function (err) {
+            self.emit("wserror", err);
+            self.ws.close(); // self.ws.emit("close");
+        });
+        this.ws.addEventListener("open", function (evt) {
+            self.pingInterval = setInterval(function () {
+                self.sendPing();
+            }, 20000);
+
+            self.noteBuffer = [];
+            self.noteBufferTime = 0;
+
+            self.noteFlushInterval = setInterval(function () {
+                if (self.noteBufferTime && self.noteBuffer.length > 0) {
+                    self.sendArray([{
+                        m: "n",
+                        t: self.noteBufferTime + self.serverTimeOffset,
+                        n: self.noteBuffer
+                    }]);
+                    self.noteBufferTime = 0;
+                    self.noteBuffer = [];
+                }
+            }, 200);
+
+            self.emit("connect");
+            self.emit("status", "Joining channel...");
+        });
+        this.ws.addEventListener("message", async function (evt) {
+            const transmission = JSON.parse(evt.data);
+            for (let i = 0; i < transmission.length; i++) {
+                const msg = transmission[i];
+                self.emit(msg.m, msg);
+            }
+        });
+    };
+
+    bindEventListeners() {
+        const self = this;
+        this.on("hi", function (msg) {
+            self.user = msg.u;
+            self.connectionTime = Date.now();
+            self.receiveServerTime(msg.t);
+
+            if (self.desiredChannelId) self.setChannel();
+
+            if (msg.permissions) {
+                self.permissions = msg.permissions;
+            } else {
+                self.permissions = {};
+            }
+        });
+        this.on("t", function (msg) {
+            self.receiveServerTime(msg.t);
+        });
+        this.on("ch", function (msg) {
+            self.desiredChannelId = msg.ch._id;
+            self.desiredChannelSettings = msg.ch.settings;
+            self.channel = msg.ch;
+            if (msg.p) self.participantId = msg.p;
+            self.setParticipants(msg.ppl);
+        });
+        this.on("p", function (msg) {
+            self.participantUpdate(msg);
+            self.emit("participant update", self.findParticipantById(msg.id));
+        });
+        this.on("m", function (msg) {
+            if (self.ppl.hasOwnProperty(msg.id)) {
+                self.participantMoveMouse(msg);
+            }
+        });
+        this.on("bye", function (msg) {
+            self.removeParticipant(msg.p);
+        });
+        this.on("b", function (msg) {
+            self.sendArray([{
+                m: "hi",
+                token: self.token
+            }]);
+        });
+    };
+
+    send(raw) {
+        if (this.isConnected()) this.ws.send(raw);
+    };
+
+    sendArray(arr) {
+        this.send(JSON.stringify(arr));
+    };
+
+    setChannel(id, set) {
+        this.desiredChannelId = id || this.desiredChannelId || "lobby";
+        this.desiredChannelSettings = set || this.desiredChannelSettings || null;
+        this.sendArray([{
+            m: "ch",
+            _id: this.desiredChannelId,
+            set: this.desiredChannelSettings
+        }]);
+    };
+
+    getChannelSetting(key) {
+        if (!this.isConnected() || !this.channel || !this.channel.settings) {
+            return this.offlineSettings.channel[key];
+        }
+        return this.channel.settings[key];
+    };
+
+    setChannelSettings(settings) {
+        if (!this.isConnected() || !this.channel || !this.channel.settings) {
+            return;
+        }
+        if (this.desiredChannelSettings) {
+            for (let key in settings) {
+                this.desiredChannelSettings[key] = settings[key];
+            }
+            this.sendArray([{
+                m: "chset",
+                set: this.desiredChannelSettings
+            }]);
+        }
+    };
+
+    getOwnParticipant() {
+        return this.findParticipantById(this.participantId);
+    };
+
+    setParticipants(ppl) {
+        // remove participants who left
+        for (const id in this.ppl) {
+            if (!this.ppl.hasOwnProperty(id)) continue;
+            let found = false;
+            for (let j = 0; j < ppl.length; j++) {
+                if (ppl[j].id === id) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                this.removeParticipant(id);
+            }
+        }
+        // update all
+        for (let i = 0; i < ppl.length; i++) {
+            this.participantUpdate(ppl[i]);
+        }
+    };
+
+    countParticipants() {
+        let count = 0;
+        for (const i in this.ppl) {
+            if (this.ppl.hasOwnProperty(i)) ++count;
+        }
+        return count;
+    };
+
+    participantUpdate(update) {
+        let part = this.ppl[update.id] || null;
+        if (part === null) {
+            part = update;
+            this.ppl[part.id] = part;
+            this.emit("participant added", part);
+            this.emit("count", this.countParticipants());
+        } else {
+            Object.keys(update).forEach(key => {
+                part[key] = update[key];
+            });
+            if (!update.tag) delete part.tag;
+            if (!update.vanished) delete part.vanished;
+        }
+    };
+
+    participantMoveMouse(update) {
+        const part = this.ppl[update.id] || null;
+        if (part !== null) {
+            part.x = update.x;
+            part.y = update.y;
+        }
+    };
+
+    removeParticipant(id) {
+        if (this.ppl.hasOwnProperty(id)) {
+            const part = this.ppl[id];
+            delete this.ppl[id];
+            this.emit("participant removed", part);
+            this.emit("count", this.countParticipants());
+        }
+    };
+
+    findParticipantById(id) {
+        return this.ppl[id] || this.offlineSettings.participant;
+    };
+
+    isOwner() {
+        return this.channel && this.channel.crown && this.channel.crown.participantId === this.participantId;
+    };
+
+    preventsPlaying() {
+        return this.isConnected() && !this.isOwner() && this.getChannelSetting("crownsolo") === true && !this.permissions.playNotesAnywhere;
+    };
+
+    receiveServerTime(time) {
+        const self = this;
+        const now = Date.now();
+        const target = time - now;
+        const duration = 1000;
+        const step = 0;
+        const steps = 50;
+        const step_ms = duration / steps;
+        const difference = target - this.serverTimeOffset;
+        const inc = difference / steps;
+        let iv;
+        iv = setInterval(function () {
+            self.serverTimeOffset += inc;
+            if (++step >= steps) {
+                clearInterval(iv);
+                self.serverTimeOffset = target;
+            }
+        }, step_ms);
+    };
+
+    startNote(note, vel) {
+        if (typeof note !== 'string') return;
+        if (this.isConnected()) {
+            vel = typeof vel === "undefined" ? undefined : +vel.toFixed(3);
+            if (!this.noteBufferTime) {
+                this.noteBufferTime = Date.now();
+                this.noteBuffer.push({
+                    n: note,
+                    v: vel
+                });
+            } else {
+                this.noteBuffer.push({
+                    d: Date.now() - this.noteBufferTime,
+                    n: note,
+                    v: vel
+                });
+            }
+        }
+    };
+
+    stopNote(note) {
+        if (typeof note !== 'string') return;
+        if (this.isConnected()) {
+            if (!this.noteBufferTime) {
+                this.noteBufferTime = Date.now();
+                this.noteBuffer.push({
+                    n: note,
+                    s: 1
+                });
+            } else {
+                this.noteBuffer.push({
+                    d: Date.now() - this.noteBufferTime,
+                    n: note,
+                    s: 1
+                });
+            }
+        }
+    };
+
+    sendPing() {
+        this.sendArray([{
+            m: "t",
+            e: Date.now()
+        }]);
+    };
+};
+
+export default Client;
